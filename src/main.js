ecb264153944d9af63de6ca66f4206c30e0adb1d
alicekb <alicekb01@gmail.com>
2021-08-13 14:45:27 -0400

    improve models and pathing

--- src/main.js
+++ src/main.js
@@ -27,6 +27,9 @@ import bucketModel from '../assets/models/bucket.stl';
 import bootModel from '../assets/models/boot.stl';
 import lampModel from '../assets/models/lamp.stl';
 import stairsModel from '../assets/models/stairs.stl';
+import straightTrackModel from '../assets/models/trackstraight.stl';
+import rightCurveTrackModel from '../assets/models/trackright.stl';
+import leftCurveTrackModel from '../assets/models/trackleft.stl';
 import boardImage from '../assets/board.jpg';
 import './styles.css';
 import Player from './player.js';
@@ -43,7 +46,13 @@ let GLOBALS = {
   currentPlayer: 0,
   currentBuildPiece: 0,
   boardSpaces: boardSpaces,
-  scene: undefined
+  scene: undefined,
+  colors: {
+      yellow: new Color3(1, .85, 0),
+      blue: new Color3(0, .3, .95),
+      red: new Color3(.95, .1, .1),
+      green: new Color3(.1, .7, .06)
+  }
 }
 
 let behavior;
@@ -68,7 +77,7 @@ const createScene = () => {
 
     rollButton.onPointerUpObservable.add(() => {
       let rollResult = diceRoller();
-      moveSpaces(Player.all[GLOBALS.currentPlayer % 4], rollResult);
+      moveSpaces(Player.all[GLOBALS.currentPlayer % GLOBALS.playerCount], rollResult);
 
     });
 
@@ -77,7 +86,7 @@ const createScene = () => {
         case KeyboardEventTypes.KEYDOWN:
             let rollResult = diceRoller();
             // this.create
-            moveSpaces(Player.all[GLOBALS.currentPlayer % 4], rollResult);
+            moveSpaces(Player.all[GLOBALS.currentPlayer % GLOBALS.playerCount], rollResult);
             break;
     }
     })
@@ -104,13 +113,7 @@ const initializeGame = (scene) => {
     board.scaling = new Vector3(2, 2, 2);
     
     SceneLoader.ImportMesh(null, '', mouseModel, scene, function(res) {
-        console.log(res);
         createPlayers(res[0]);
-        // res[0].scaling = new BABYLON.Vector3(0.007, 0.007, 0.007)
-        
-        // const playerOne = new Player(mouseMesh, 0, 0, 'playerOne')
-        // playerOne.mesh.position = new BABYLON.Vector3(-8, 0, 1.7)
-        //     createPlayers(res[0]);
             
         
         // const pointerDrag = new PointerDragBehavior({ dragPlaneNormal: new Vector3(0, 1, 0) });
@@ -130,15 +133,20 @@ const initializeGame = (scene) => {
     });
     base.position = new Vector3(-0.5, 0, -6);
     base.rotation.y = Math.PI / 2.25;
+    const baseMaterial = new StandardMaterial("baseMaterial");
+    baseMaterial.diffuseColor = GLOBALS.colors.yellow;
+    base.material = baseMaterial;
 
-    const gear_support = BoxBuilder.CreateBox("gear_support", {
+    const gearSupport = BoxBuilder.CreateBox("gear_support", {
         height: 2,
         width: 0.8,
         depth: 0.2
     });
-
-    gear_support.position = new Vector3(4, 1, -5.3);
-    gear_support.rotation.y = -Math.PI / 12;
+    gearSupport.position = new Vector3(4, 1, -5.3);
+    gearSupport.rotation.y = -Math.PI / 12;
+    const gearBaseMaterial = new StandardMaterial("gearBaseMaterial");
+    gearBaseMaterial.diffuseColor = GLOBALS.colors.blue;
+    gearSupport.material = gearBaseMaterial;
 
     SceneLoader.ImportMesh(null, "", crankModel, scene, function(res) {
         const crank = res[0];
@@ -146,7 +154,11 @@ const initializeGame = (scene) => {
         crank.position = new Vector3(3.95, 1.75, -5.4);
         crank.rotation.z = Math.PI / 2;
         crank.rotation.y = Math.PI / 2.3;
+        const crankMaterial = new StandardMaterial("crankMaterial");
+        crankMaterial.diffuseColor = GLOBALS.colors.green;
+        crank.material = crankMaterial;
    });
+   
 
    SceneLoader.ImportMesh(null, "", gearyModel, scene, function(res) {
     const gearOne = res[0];
@@ -154,6 +166,9 @@ const initializeGame = (scene) => {
     gearOne.position = new Vector3(3.8, 1.3, -5.3);
     gearOne.rotation.z = Math.PI / 2;
     gearOne.rotation.y = Math.PI / 2.3;
+    const gearyOneMaterial = new StandardMaterial("gearyOneMaterial");
+    gearyOneMaterial.diffuseColor = GLOBALS.colors.red;
+    gearOne.material = gearyOneMaterial;
 });
 
     SceneLoader.ImportMesh(null, "", gearyModel, scene, function(res) {
@@ -162,6 +177,9 @@ const initializeGame = (scene) => {
         gearTwo.position = new Vector3(3.6, 1.3, -3.8);
         gearTwo.rotation.z = 0;
         gearTwo.rotation.y = Math.PI / 2.3;
+        const gearyTwoMaterial = new StandardMaterial("gearyTwoMaterial");
+        gearyTwoMaterial.diffuseColor = GLOBALS.colors.yellow;
+        gearTwo.material = gearyTwoMaterial;
     });
 
     //paired with stop
@@ -173,6 +191,9 @@ const initializeGame = (scene) => {
     stopArm.position = new Vector3(-1.75, 1.5, -5)
     stopArm.rotation.z = Math.PI / 2;
     stopArm.rotation.y = Math.PI / 2.2;
+    const stopArmMaterial = new StandardMaterial("stopArmMaterial");
+    stopArmMaterial.diffuseColor = GLOBALS.colors.red;
+    stopArm.material = stopArmMaterial;
     SceneLoader.ImportMesh(null, "", stopModel, scene, function(res) {
         const stopSign = res[0];
         stopSign.scaling = new Vector3(0.02, 0.02, 0.02);
@@ -180,6 +201,9 @@ const initializeGame = (scene) => {
         stopSign.rotation.z = Math.PI / 2;
         stopSign.rotation.x = -Math.PI / 2;
         stopSign.rotation.y = Math.PI / 2.25;
+        const stopSignMaterial = new StandardMaterial("stopSignMaterial");
+        stopSignMaterial.diffuseColor = GLOBALS.colors.red;
+        stopSign.material = stopSignMaterial;
     });
 
     SceneLoader.ImportMesh(null, "", lampModel, scene, function(res) {
@@ -189,6 +213,9 @@ const initializeGame = (scene) => {
         lamp.rotation.z = Math.PI / 2;
         lamp.rotation.x = -Math.PI / 2;
         lamp.rotation.y = Math.PI / 2;
+        const lampMaterial = new StandardMaterial("lampMaterial");
+        lampMaterial.diffuseColor = GLOBALS.colors.yellow;
+        lamp.material = lampMaterial;
     });
 
     //paired with boot
@@ -199,11 +226,17 @@ const initializeGame = (scene) => {
     });
     bootArm.position = new Vector3(-5.8, 3.5, -4.95)
     bootArm.rotation.y = Math.PI / 2.2;
+    const bootArmMaterial = new StandardMaterial("bootArmMaterial");
+    bootArmMaterial.diffuseColor = GLOBALS.colors.green;
+    bootArm.material = bootArmMaterial;
     SceneLoader.ImportMesh(null, "", bootModel, scene, function(res) {
-        const lamp = res[0];
-        lamp.scaling = new Vector3(0.01, 0.01, 0.01);
-        lamp.position = new Vector3(-5.8, 2.5, -4.3);
-        lamp.rotation.y = -Math.PI / 4;
+        const boot = res[0];
+        boot.scaling = new Vector3(0.01, 0.01, 0.01);
+        boot.position = new Vector3(-5.8, 2.5, -4.3);
+        boot.rotation.y = -Math.PI / 4;
+        const bootMaterial = new StandardMaterial("bootMaterial");
+        bootMaterial.diffuseColor = GLOBALS.colors.green;
+        boot.material = bootMaterial;
     });
 
     SceneLoader.ImportMesh(null, "", stairsModel, scene, function(res) {
@@ -211,12 +244,18 @@ const initializeGame = (scene) => {
         stairsOne.scaling = new Vector3(0.1, 0.1, 0.05);
         stairsOne.position = new Vector3(-5.1, 1.25, -2);
         stairsOne.rotation.y = Math.PI / 20;
+        const stairsOneMaterial = new StandardMaterial("stairsOneMaterial");
+        stairsOneMaterial.diffuseColor = GLOBALS.colors.blue;
+        stairsOne.material = stairsOneMaterial;
     });
     SceneLoader.ImportMesh(null, "", stairsModel, scene, function(res) {
         const stairsTwo = res[0];
         stairsTwo.scaling = new Vector3(0.1, 0.1, 0.05);
         stairsTwo.position = new Vector3(-4.75, 0.75, -.5);
         stairsTwo.rotation.y = Math.PI / 20;
+        const stairsTwoMaterial = new StandardMaterial("stairsTwoMaterial");
+        stairsTwoMaterial.diffuseColor = GLOBALS.colors.blue;
+        stairsTwo.material = stairsTwoMaterial;
     });
     const stairLegOne = BoxBuilder.CreateBox("stairLegOne", {
         height: 2,
@@ -242,16 +281,28 @@ const initializeGame = (scene) => {
         depth: 0.25
     });
     stairLegFour.position = new Vector3(-3.9, 0.375, 0.5)
+    const stairsLegsMaterial = new StandardMaterial("stairsLegsMaterial");
+    stairsLegsMaterial.diffuseColor = GLOBALS.colors.blue;
+    stairLegOne.material = stairsLegsMaterial;
+    stairLegTwo.material = stairsLegsMaterial;
+    stairLegThree.material = stairsLegsMaterial;
+    stairLegFour.material = stairsLegsMaterial;
 
     SceneLoader.ImportMesh(null, "", bucketModel, scene, function(res) {
-        const stairsTwo = res[0];
-        stairsTwo.scaling = new Vector3(0.005, 0.005, 0.005);
-        stairsTwo.position = new Vector3(-6.2, 2.175, -3.4);
-        stairsTwo.rotation.y = Math.PI / 20;
+        const bucket = res[0];
+        bucket.scaling = new Vector3(0.005, 0.005, 0.005);
+        bucket.position = new Vector3(-6.2, 2.175, -3.4);
+        bucket.rotation.y = Math.PI / 20;
+        const bucketMaterial = new StandardMaterial("bucketMaterial");
+        bucketMaterial.diffuseColor = GLOBALS.colors.yellow;
+        bucket.material = bucketMaterial;
     });
 
     const marble = SphereBuilder.CreateSphere("marble", { diameter: 0.5 });
     marble.position = new Vector3(-5.72, 2.4, -3.15)
+    const marbleMaterial = new StandardMaterial("marbleMaterial");
+    marbleMaterial.diffuseColor = GLOBALS.colors.yellow;
+    marble.material = marbleMaterial;
 
     const baseTwo = BoxBuilder.CreateBox("baseTwo", {
         height: 0.2,
@@ -260,11 +311,90 @@ const initializeGame = (scene) => {
     });
     baseTwo.position = new Vector3(5.8, 0, 2);
     baseTwo.rotation.y = -5.5*Math.PI/6;
+    const baseTwoMaterial = new StandardMaterial("baseTwoMaterial");
+    baseTwoMaterial.diffuseColor = GLOBALS.colors.yellow;
+    baseTwo.material = baseTwoMaterial;
 
-    camera.setTarget(board.position)
+    let trackPartOne = null;
+    const trackMaterial = new StandardMaterial("trackMaterial");
+    trackMaterial.diffuseColor = GLOBALS.colors.red;
+    SceneLoader.ImportMesh(null, "", rightCurveTrackModel, scene, function(res) {
+        trackPartOne = res[0];
+        trackPartOne.scaling = new Vector3(0.0125, 0.0125, 0.0125);
+        trackPartOne.position = new Vector3(-6.2, 1.1, 1);
+        trackPartOne.rotation.y = Math.PI / 20;
+        trackPartOne.rotation.x = Math.PI / 2;
 
-    GLOBALS.camera = camera
-    GLOBALS.board = board
+        trackPartOne.material = trackMaterial;
+
+        
+    SceneLoader.ImportMesh(null, "", rightCurveTrackModel, scene, function(res) {
+        const trackPartTwo = res[0];
+        trackPartTwo.scaling = new Vector3(0.0125, 0.0125, 0.0125);
+        trackPartTwo.position = new Vector3(-4.2, 1.1, 2.75);
+        trackPartTwo.rotation.y = Math.PI / 2;
+        trackPartTwo.rotation.x = Math.PI / 2;
+
+        trackPartTwo.material = trackMaterial;
+
+        trackPartOne.addChild(trackPartTwo);
+    });
+    SceneLoader.ImportMesh(null, "", leftCurveTrackModel, scene, function(res) {
+        const trackPartThree = res[0];
+        trackPartThree.scaling = new Vector3(0.0125, 0.0125, 0.0125);
+        trackPartThree.position = new Vector3(-0.5, 1.1, 0.65);
+        trackPartThree.rotation.y = Math.PI;
+        trackPartThree.rotation.x = Math.PI / 2;
+
+        trackPartThree.material = trackMaterial;
+
+        trackPartOne.addChild(trackPartThree);
+    });
+    SceneLoader.ImportMesh(null, "", straightTrackModel, scene, function(res) {
+        const trackPartFour = res[0];
+        trackPartFour.scaling = new Vector3(0.0125, 0.0125, 0.0125);
+        trackPartFour.position = new Vector3(-.75, 1.1, -0.93);
+        trackPartFour.rotation.y = Math.PI / 2;
+        trackPartFour.rotation.x = Math.PI / 2;
+
+        trackPartFour.material = trackMaterial;
+
+        trackPartOne.addChild(trackPartFour);
+    });
+    SceneLoader.ImportMesh(null, "", straightTrackModel, scene, function(res) {
+        const trackPartFive = res[0];
+        trackPartFive.scaling = new Vector3(0.0125, 0.0125, 0.0125);
+        trackPartFive.position = new Vector3(1.1, 1.1, -0.93);
+        trackPartFive.rotation.y = Math.PI / 2;
+        trackPartFive.rotation.x = Math.PI / 2;
+
+        trackPartFive.material = trackMaterial;
+
+        trackPartOne.addChild(trackPartFive);
+    });
+    SceneLoader.ImportMesh(null, "", straightTrackModel, scene, function(res) {
+        const trackPartSix = res[0];
+        trackPartSix.scaling = new Vector3(0.0125, 0.0125, 0.0125);
+        trackPartSix.position = new Vector3(2.95, 1.1, -0.93);
+        trackPartSix.rotation.y = Math.PI / 2;
+        trackPartSix.rotation.x = Math.PI / 2;
+
+        trackPartSix.material = trackMaterial;
+        
+        trackPartOne.addChild(trackPartSix);
+
+        trackPartOne.addRotation(0, -Math.PI / 35, 0);
+
+        
+        marble.material = marbleMaterial;
+    });
+    });
+    
+
+    camera.setTarget(board.position);
+
+    GLOBALS.camera = camera;
+    GLOBALS.board = board;
 }
 
 const createPlayers = (mouseMesh) => {
@@ -280,6 +410,11 @@ const createPlayers = (mouseMesh) => {
 }
 
 const moveSpaces = (player, spaceCount) => {
+    let newSpaceIndex = player.currentSpace + spaceCount;
+    if (newSpaceIndex > GLOBALS.boardSpaces.length) {
+        let startSpace = 44; //The SAFE space
+        newSpaceIndex = startSpace + spaceCount;
+    }
 
     const endSpace = adjustPlayerPositions(GLOBALS.boardSpaces[player.currentSpace + spaceCount], player.currentSpace + spaceCount);
     for (let i = player.currentSpace; i < player.currentSpace + spaceCount; i++) {
@@ -305,14 +440,14 @@ const turnPlayer = (player) => {
 }
 
 const resolveSpace = (space) => {
-    if (space.type == "build" && space.body.includes("4")) {
+    if (space.type == "build" && space.body.includes(GLOBALS.playerCount.toString())) {
         buildNewPiece();
     }
     else if(space.type == "goback") {
-        moveSpaces(Player.all[GLOBALS.currentPlayer % 4], -space.spaces);
+        moveSpaces(Player.all[GLOBALS.currentPlayer % GLOBALS.playerCount], -space.spaces);
     }
     else if (space.type == "goforward") {
-        moveSpaces(Player.all[GLOBALS.currentPlayer % 4], space.spaces);
+        moveSpaces(Player.all[GLOBALS.currentPlayer % GLOBALS.playerCount], space.spaces);
     }
 
     GLOBALS.currentPlayer++;
